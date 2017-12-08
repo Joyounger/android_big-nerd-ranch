0ab9b65dcfd21904141582a63f397dcb0418dd1f
Joyounger <942510346@qq.com>
2017-12-08 16:58:44 +0800

    finish 12.3.1 传递数据给 DatePickerFragment
    Signed-off-by: Joyounger <942510346@qq.com>

--- ch12DateDialog/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ ch12DateDialog/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -7,16 +7,43 @@ import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by linyang on 2017/12/8.
  */
 
 public class DatePickerFragment extends DialogFragment {
+    private static final String ARG_DATE = "date";
+    private DatePicker mDatePicker;
+
+    //要传递crime记录日期给 DatePickerFragment ，需将它保存在 DatePickerFragment 的 argument bundle中
+    public static DatePickerFragment newInstance(Date date) { //创建和设置fragment argument通常是在 newInstance() 方法中完成的
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_DATE, date);
+        DatePickerFragment fragment = new DatePickerFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Date date = (Date) getArguments().getSerializable(ARG_DATE);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
         View v = LayoutInflater.from(getActivity())
                 .inflate(R.layout.dialog_date, null);
+        
+        mDatePicker = (DatePicker) v.findViewById(R.id.dialog_date_date_picker);
+        mDatePicker.init(year, month, day, null);
 
         return new AlertDialog.Builder(getActivity())  //使用 AlertDialog.Builder 类，我们以流接口（fluent interface）的方式创建了 AlertDialog 实例
                 .setView(v)
