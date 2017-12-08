00d58d24947dddbc5a07b62b38e68ef2ab1a639e
Joyounger <942510346@qq.com>
2017-12-08 16:27:10 +0800

    finish 12.2.2 设置对话框的显示内容
    Signed-off-by: Joyounger <942510346@qq.com>

--- ch12DateDialog/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ ch12DateDialog/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -5,6 +5,8 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * Created by linyang on 2017/12/8.
@@ -13,7 +15,11 @@ import android.support.v7.app.AlertDialog;
 public class DatePickerFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        View v = LayoutInflater.from(getActivity())
+                .inflate(R.layout.dialog_date, null);
+
         return new AlertDialog.Builder(getActivity())  //使用 AlertDialog.Builder 类，我们以流接口（fluent interface）的方式创建了 AlertDialog 实例
+                .setView(v)
                 .setTitle(R.string.date_picker_title)
                 .setPositiveButton(android.R.string.ok, null)
                 .create();
