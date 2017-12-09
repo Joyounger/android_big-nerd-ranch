e4555ae966410fd00534a457ea308e32b6e41cbf
Joyounger <942510346@qq.com>
2017-12-09 09:55:14 +0800

    finish 12.3.2 返回数据给 CrimeFragment
    Signed-off-by: Joyounger <942510346@qq.com>

--- ch12DateDialog/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
+++ ch12DateDialog/CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -1,7 +1,10 @@
 package com.bignerdranch.android.criminalintent;
 
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
@@ -11,6 +14,7 @@ import android.widget.DatePicker;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Created by linyang on 2017/12/8.
@@ -19,6 +23,8 @@ import java.util.Date;
 public class DatePickerFragment extends DialogFragment {
     private static final String ARG_DATE = "date";
     private DatePicker mDatePicker;
+    public static final String EXTRA_DATE =
+            "com.bignerdranch.android.criminalintent.date";
 
     //要传递crime记录日期给 DatePickerFragment ，需将它保存在 DatePickerFragment 的 argument bundle中
     public static DatePickerFragment newInstance(Date date) { //创建和设置fragment argument通常是在 newInstance() 方法中完成的
@@ -41,14 +47,35 @@ public class DatePickerFragment extends DialogFragment {
 
         View v = LayoutInflater.from(getActivity())
                 .inflate(R.layout.dialog_date, null);
-        
+
         mDatePicker = (DatePicker) v.findViewById(R.id.dialog_date_date_picker);
         mDatePicker.init(year, month, day, null);
 
         return new AlertDialog.Builder(getActivity())  //使用 AlertDialog.Builder 类，我们以流接口（fluent interface）的方式创建了 AlertDialog 实例
                 .setView(v)
                 .setTitle(R.string.date_picker_title)
-                .setPositiveButton(android.R.string.ok, null)
+                //.setPositiveButton(android.R.string.ok, null)
+                .setPositiveButton(android.R.string.ok,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                int year = mDatePicker.getYear();
+                                int month = mDatePicker.getMonth();
+                                int day = mDatePicker.getDayOfMonth();
+                                Date date = new GregorianCalendar(year, month, day).getTime();
+                                sendResult(Activity.RESULT_OK, date);
+                            }
+                 })
                 .create();
+    }
+
+    private void sendResult(int resultCode, Date date) {
+        if (getTargetFragment() == null) {
+            return;
+        }
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_DATE, date);
+        getTargetFragment()
+                .onActivityResult(getTargetRequestCode(), resultCode, intent);
     }
 }
